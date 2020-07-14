2d11e489c84c4b493ef6a84e0914a454df6376a5
wwy <1257426549@qq.com>
2020-07-14 17:05:48 +0800

    add

--- admin/src/views/Main.vue
+++ admin/src/views/Main.vue
@@ -1,106 +1,98 @@
 <template>
-    <el-container style="height: 100vh;">
-    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu router  unique-opened :default-active="$route.path">
-        <el-submenu index="1">
-          <template slot="title"><i class="el-icon-message"></i>内容管理</template>
-          
-
-           <el-menu-item-group>
-            <template slot="title">物品</template>
-            <el-menu-item index="/items/create">新建物品</el-menu-item>
-            <el-menu-item index="/items/list">物品列表</el-menu-item>
-          </el-menu-item-group>
-
-          <el-menu-item-group>
-            <template slot="title">英雄</template>
-            <el-menu-item index="/heroes/create">新建英雄</el-menu-item>
-            <el-menu-item index="/heroes/list">英雄列表</el-menu-item>
-          </el-menu-item-group>
-
-
-          <el-menu-item-group>
-            <template slot="title">文章</template>
-            <el-menu-item index="/articles/create">新建文章</el-menu-item>
-            <el-menu-item index="/articles/list">文章列表</el-menu-item>
-          </el-menu-item-group>
-
-          
-        </el-submenu>
-
-
-
-        <el-submenu index="2">
-          <template slot="title"><i class="el-icon-message"></i>运营管理</template>
-
-
-          <el-menu-item-group>
-            <template slot="title">广告位</template>
-            <el-menu-item index="/ads/create">新建广告位</el-menu-item>
-            <el-menu-item index="/ads/list">广告位列表</el-menu-item>
-          </el-menu-item-group>
-          
-        </el-submenu>
-
-        <el-submenu index="3">
-          <template slot="title"><i class="el-icon-message"></i>系统设置</template>
-          <el-menu-item-group>
-            <template slot="title">分类</template>
-            <el-menu-item index="/categories/create">新建分类</el-menu-item>
-            <el-menu-item index="/categories/list">分类列表</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group>
-            <template slot="title">管理员</template>
-            <el-menu-item index="/admin_users/create">新建管理员</el-menu-item>
-            <el-menu-item index="/admin_users/list">管理员列表</el-menu-item>
-          </el-menu-item-group>
-
-        </el-submenu>
-
-      </el-menu>
-    </el-aside>
-    
-    <el-container>
+    <el-container style="height: 100vh">
+      <!-- 头部 -->
       <el-header style="text-align: right; font-size: 12px">
-        <el-dropdown>
-          <i class="el-icon-setting" style="margin-right: 15px"></i>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>查看</el-dropdown-item>
-            <el-dropdown-item>新增</el-dropdown-item>
-            <el-dropdown-item>删除</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-        <span>王小虎</span>
+      <div class="logo-title">
+          <img src="../assets/img/home/logo1.png" alt="王者荣耀">
+          <h2>王者荣耀后台管理系统</h2>
+      </div>
+        <el-button type="text" size="middle" style="color:grey" @click="loginOut"> <i class="el-icon-switch-button" style="color:#fdb933"></i> 退出</el-button>
       </el-header>
-      
-      <el-main>
 
-        <router-view></router-view>
-        <!-- <el-table :data="tableData">
-          <el-table-column prop="date" label="日期" width="140">
-          </el-table-column>
-          <el-table-column prop="name" label="姓名" width="120">
-          </el-table-column>
-          <el-table-column prop="address" label="地址">
-          </el-table-column>
-        </el-table> -->
-      </el-main>
-    </el-container>
+
+      <el-container>
+         <!-- 侧边栏菜单 -->
+          <el-aside width="201px" style="background-color: #252a40">
+             <el-menu router :default-active="activePath" unique-opened
+        background-color="#252a40" text-color="#fff" active-text-color="#fdb933">
+               <!-- 内容管理一级菜单 -->
+              <el-submenu index="1">
+                <template slot="title"><i class="el-icon-message"></i>分类管理</template>
+                <!-- 内容管理二级菜单 -->
+                <el-menu-item-group>
+                  <template slot="title">分类</template>
+                  <el-menu-item index="/categories/create">新建分类</el-menu-item>
+                  <el-menu-item index="/categories/list">分类列表</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+              <!-- 物品管理一级菜单 -->
+              <el-submenu index="2">
+                <template slot="title"><i class="el-icon-message"></i>物品管理</template>
+                 <!-- 物品管理二级菜单 -->
+                <el-menu-item-group>
+                  <template slot="title">物品</template>
+                  <el-menu-item index="/items/create">新建物品</el-menu-item>
+                  <el-menu-item index="/items/list">物品列表</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+                
+              <!-- 英雄管理一级菜单 -->
+              <el-submenu index="3">
+                <template slot="title"><i class="el-icon-message"></i>英雄管理</template>
+                <!-- 英雄管理二级菜单 -->
+                <el-menu-item-group>
+                  <template slot="title">英雄</template>
+                  <el-menu-item index="/heroes/create">新建英雄</el-menu-item>
+                  <el-menu-item index="/heroes/list">英雄列表</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+
+              <!-- 文章管理 -->
+              <el-submenu index="4">
+                <template slot="title"><i class="el-icon-message"></i>文章管理</template>
+                <!--文章管理二级菜单 -->
+                <el-menu-item-group>
+                  <template slot="title">文章</template>
+                  <el-menu-item index="/articles/create">新建文章</el-menu-item>
+                  <el-menu-item index="/articles/list">文章列表</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+
+              <!-- 广告位管理 -->
+              <el-submenu index="5">
+                <template slot="title"><i class="el-icon-message"></i>运营管理</template>
+                <el-menu-item-group>
+                  <template slot="title">广告位</template>
+                  <el-menu-item index="/ads/create">新建广告位</el-menu-item>
+                  <el-menu-item index="/ads/list">广告位列表</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+
+               <!-- 系统设置 -->
+              <el-submenu index="6">
+                <template slot="title"><i class="el-icon-message"></i>系统设置</template>
+                <el-menu-item-group>
+                  <template slot="title">管理员</template>
+                  <el-menu-item index="/admin_users/create">新建管理员</el-menu-item>
+                  <el-menu-item index="/admin_users/list">管理员列表</el-menu-item>
+                </el-menu-item-group>
+
+              </el-submenu>
+
+            </el-menu>
+          </el-aside>
+    
+              <!-- 内容主体区域 -->
+          <el-main>
+            <router-view></router-view>
+          </el-main>
   </el-container>
+
+    </el-container>
+
+    
 </template>
 
-
-<style>
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    color: #333;
-  }
-</style>
 
 <script>
   export default {
@@ -116,3 +108,20 @@
     }
   };
 </script>
+
+
+<style scoped>
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: linear-gradient(#11131f, #252a40);
+  color: #eee;
+}
+.logo-title{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+</style>
