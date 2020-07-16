d9b59b051ff181c72e2f731ce8aeea79279a005b
wwy <1257426549@qq.com>
2020-07-16 18:11:53 +0800

    web开发

--- web/src/main.js
+++ web/src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import "./style.scss";
+import router from './router'
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+
+// If you use Swiper 6.0.0 or higher
+import 'swiper/swiper-bundle.css';
+ 
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 
 Vue.config.productionTip = false
 
 
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ web/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <swiper>
+        <swiper-slide>
+          <img src="../assets/images/banner.jpeg" alt="">
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/images/banner02.jpeg" alt="">
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/images/banner03.jpeg" alt="">
+        </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
