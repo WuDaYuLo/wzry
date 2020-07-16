c4baaf86e426bc76c8ffe7c5b3b9c6872b737fd8
DaveWu <1257426549@qq.com>
2020-07-16 23:55:46 +0800

    增加轮播图

--- web/src/main.js
+++ web/src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import "./style.scss";
+import "./assets/scss/style.scss";
 import router from './router'
 
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 
 // If you use Swiper 6.0.0 or higher
-import 'swiper/swiper-bundle.css';
+import 'swiper/dist/css/swiper.css'
  
 Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
 
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <swiper>
+    <swiper :options="swiperOptions" id="parent">
         <swiper-slide>
-          <img src="../assets/images/banner.jpeg" alt="">
+          <img class="w-100" src="../assets/images/banner.jpeg" alt="">
         </swiper-slide>
         <swiper-slide>
-          <img src="../assets/images/banner02.jpeg" alt="">
+          <img class="w-100" src="../assets/images/banner02.jpeg" alt="">
         </swiper-slide>
         <swiper-slide>
-          <img src="../assets/images/banner03.jpeg" alt="">
+          <img class="w-100" src="../assets/images/banner03.jpeg" alt="">
         </swiper-slide>
+        <div class="swiper-pagination pagination-home text-right px-3 pb-1" 
+        slot="pagination"></div>
     </swiper>
   </div>
 </template>
@@ -17,9 +19,39 @@
 <script>
 
 export default {
-  name: 'Home',
-  components: {
-
-  }
+  data(){
+    return {
+      swiperOptions:{
+        pagination: {
+          el: '.pagination-home',
+         //循环
+          loop:true,
+          //每张播放时长3秒，自动播放
+          autoplay:2000,
+          //滑动速度
+          speed:1000,
+          // delay:1000
+        },
+      }
+    }
+  }  
 }
 </script>
+
+
+<style lang="scss">
+
+@import "../assets/scss/variables";
+
+.pagination-home {
+  .swiper-pagination-bullet {
+    opacity: 1;
+    border-radius: 0.1538rem;
+    background-color: map-get($colors, 'white');
+    &.swiper-pagination-bullet-active {
+        background: map-get($colors, 'info' )
+    }
+  }
+  
+}
+</style>
